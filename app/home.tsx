a9e941488f97d2b42891c393aa9050376f5d4c39
Sumit262601 <skr.04088s@gmail.com>
2025-03-30 14:32:23 +0530

    home.tsx: Working on progress bar

---
+++ app/home.tsx
@@ -0,0 +1,111 @@
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import { useState, useEffect, useRef, useCallback } from "react";
+import { View, Text, ScrollView, TouchableOpacity, Animated, Dimensions, StyleSheet } from "react-native";
+import Svg, { Circle } from "react-native-svg";
+
+const { width } = Dimensions.get('window');
+
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+
+interface CircularProgressProps {
+    progress: number;
+    totalDoses: number;
+    completedDoses: number;
+}
+
+function CircularProgress({
+    progress,
+    totalDoses,
+    completedDoses
+}: CircularProgressProps) {
+    const animationValue = useRef(new Animated.Value(0)).current;
+    const size = width * 0.55;
+    const strokeWidth = 15;
+    const radius = (size - strokeWidth) / 2;
+    const circumference = 2 * Math.PI * radius;
+
+    useEffect(() => {
+        Animated.timing(animationValue, {
+            toValue: progress,
+            duration: 1000,
+            useNativeDriver: true,
+        }).start();
+
+    }, [progress]);
+
+    const strokeDashffest = animationValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: [circumference, 0]
+    });
+
+    return (
+        <View>
+            <View>
+                <Text>{Math.round(progress)}%</Text>
+                <Text>{completedDoses} of {totalDoses} doeses</Text>
+            </View>
+            <Svg width={size} height={size}>
+                <Circle
+                    cx={size / 2}
+                    cy={size / 2}
+                    r={radius}
+                    stroke="rgba(255, 255, 255, 0.2)"
+                    strokeWidth={strokeWidth}
+                    fill="none"
+                />
+                <AnimatedCircle
+                    cx={size / 2}
+                    cy={size / 2}
+                    r={radius}
+                    stroke="white"
+                    strokeWidth={strokeWidth}
+                    fill="none"
+                    strokeDasharray={circumference}
+                    strokeDashoffset={strokeDashffest}
+                    strokeLinecap="round"
+                    transform={`rotate(-90 ${size / 2} ${size / 2})`}
+                />
+            </Svg>
+        </View>
+    )
+}
+
+export default function HomeScreen() {
+    return (
+        <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
+            <LinearGradient colors={["#1a8e2d", "#146922"]} style={styles.header}>
+                <View>
+                    <View>
+                        <View>
+                            <Text>Daily Progress</Text>
+                        </View>
+                        <TouchableOpacity>
+                            <Ionicons name="notifications-outline" size={24} color="white" />
+                        </TouchableOpacity>
+                    </View>
+                    {/* Circular Progress bar */}
+                    <CircularProgress progress={70} totalDoses={10} completedDoses={5} />
+                </View>
+            </LinearGradient>
+        </ScrollView>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#f8f9fa",
+    },
+    header: {
+        paddingTop: 50,
+        paddingBottom: 25,
+        borderBottomLeftRadius: 30,
+        borderBottomRightRadius: 30,
+    },
+    headerContent: {
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    }
+})
