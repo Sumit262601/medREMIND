ee2eeaacd405dca7bf3289ee05ba7fb93fe2a9df
Sumit262601 <skr.04088s@gmail.com>
2025-04-09 17:29:33 +0530

    Add Medication pages is complet One half

--- app/medications/add.tsx
+++ app/medications/add.tsx
@@ -47,9 +47,11 @@ const DURATIONS = [
 ]
 
 export default function AddMedicationScreen() {
+    const [showDatePicker, setShowDatePicker] = useState(false);
+    const [showTimePicker, setShowTimePicker] = useState(false);
 
     const [form, setForm] = useState({
-        naem: "",
+        name: "",
         dosage: "",
         frequency: "",
         duration: "",
@@ -58,8 +60,25 @@ export default function AddMedicationScreen() {
         notes: "",
         reminderEnabled: true,
         refillReminder: false,
+        currentSupply: "",
         refillAt: "",
     });
+
+    const onDateChange = (event: any, selectedDate?: Date) => {
+        setShowDatePicker(Platform.OS === 'ios');
+        if (selectedDate) {
+            setForm(prev => ({ ...prev, startDate: selectedDate }));
+        }
+    };
+
+    const onTimeChange = (event: any, selectedTime?: Date) => {
+        setShowTimePicker(Platform.OS === 'ios');
+        if (selectedTime) {
+            const hours = selectedTime.getHours().toString().padStart(2, '0');
+            const minutes = selectedTime.getMinutes().toString().padStart(2, '0');
+            setForm(prev => ({ ...prev, times: [`${hours}:${minutes}`] }));
+        }
+    };
 
     const renderFrequencyOptions = () => {
         return (
@@ -101,23 +120,27 @@ export default function AddMedicationScreen() {
         )
     };
     return (
-        <View>
-            {/* */}
+        <View style={styles.container}>
+            {/* Medication add */}
             <LinearGradient
-                colors={["#1a8e2d", "146922"]}
+                colors={["#1a8e2d", "#146922"]}
                 start={{ x: 0, y: 0 }}
                 end={{ x: 1, y: 1 }}
+                style={styles.headerGradient}
             />
-            <View>
-                <View>
-                    <TouchableOpacity>
+            <View style={styles.content}> 
+                <View style={styles.header}>
+                    <TouchableOpacity style={styles.backButton}>
                         <Ionicons name="chevron-back" size={28} color={'#1a8e2d'} />
                     </TouchableOpacity>
-                    <Text>New Medication</Text>
+                    <Text style={styles.headerTitle}>New Medication</Text>
                 </View>
 
                 {/*  */}
-                <ScrollView showsHorizontalScrollIndicator={false}>
+                <ScrollView showsHorizontalScrollIndicator={false} 
+                    style={{ flex: 1}}
+                    contentContainerStyle={styles.formContentContainer}
+                >
                     {/* basic informations to add medications */}
                     <View>
                         <View>
@@ -132,39 +155,177 @@ export default function AddMedicationScreen() {
                                 placeholderTextColor={'#999'}
                             />
                         </View>
-                        <View>
-                            <Text>How often?</Text>
-                            {renderFrequencyOptions()}
-                            {/* render frequency options */}
+                    </View>
 
-                            <Text>For how long?</Text>
-                            {renderDurationOptions()}
-                            {/* rebder duration options */}
+                    {/* schedule */}
+                    <View>
+                        <Text>How often?</Text>
+                        {renderFrequencyOptions()}
+                        {/* render frequency options */}
 
-                            <TouchableOpacity>
-                                <View>
-                                    <Ionicons name="calendar" size={24} color={"#1a8e2d"} />
-                                </View>
-                                <Text>Starts: { }</Text>
-                            </TouchableOpacity>
+                        <Text>For how long?</Text>
+                        {renderDurationOptions()}
+                        {/* rebder duration options */}
 
+                        <TouchableOpacity onPress={() => setShowDatePicker(true)}>
+                            <View>
+                                <Ionicons name="calendar" size={24} color={"#1a8e2d"} />
+                            </View>
+                            <Text>Starts: {form.startDate.toLocaleDateString()}</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => setShowTimePicker(true)}>
+                            <View>
+                                <Ionicons name="time" size={24} color={"#1a8e2d"} />
+                            </View>
+                            <Text>Time: {form.times[0]}</Text>
+                        </TouchableOpacity>
+
+                        {/* <DateTimePicker
+                            value={form.startDate}
+                            mode="date"
+                        />
+                        <DateTimePicker
+                            mode="time"
+                            value={(() => {
+                                const [hours, mintues] = form.times[0].split(":").map(Number);
+                                const date = new Date();
+                                date.setHours(hours, mintues, 0, 0);
+                                return date;
+                            })()}
+                        /> */}
+
+                        {showDatePicker && (
                             <DateTimePicker
+                                testID="datePicker"
                                 value={form.startDate}
                                 mode="date"
+                                is24Hour={true}
+                                onChange={onDateChange}
+                                display={Platform.OS === 'ios' ? 'spinner' : 'default'}
                             />
-                            <DateTimePicker 
+                        )}
+
+                        {showTimePicker && (
+                            <DateTimePicker
                                 mode="time"
                                 value={(() => {
-                                    const [hours, mintues] = form.times[0].split(":").map(Number);
+                                    const [hours, minutes] = form.times[0].split(":").map(Number);
                                     const date = new Date();
-                                    date.setHours(hours, mintues, 0, 0);
+                                    date.setHours(hours, minutes, 0, 0);
                                     return date;
                                 })()}
+                                onChange={onTimeChange}
+                                display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+                            />
+                        )}
+                    </View>
+
+                    {/* Reminder */}
+                    <View>
+                        <View>
+                            <View>
+                                <View>
+                                    <View>
+                                        <Ionicons name="notifications" ccolor={"#1a8e2d"} />
+                                    </View>
+                                    <View>
+                                        <Text>Reminder</Text>
+                                        <Text>
+                                            Get notified when its time to take your medications
+                                        </Text>
+                                    </View>
+                                </View>
+                                <Switch
+                                    trackColor={{ false: "#ddd", true: "#1a8e2d" }}
+                                    thumbColor={'white'}
+                                />
+                            </View>
+                        </View>
+                    </View>
+
+                    {/* Refill Tracking */}
+
+                    {/* Notes */}
+                    <View>
+                        <View>
+                            <TextInput
+                                placeholder="Add notes or special instructions..."
+                                placeholderTextColor='#999'
                             />
                         </View>
                     </View>
+
+
                 </ScrollView>
+
+                <View>
+                    <TouchableOpacity>
+                        <LinearGradient
+                            colors={["#1a8e2d", "#146922"]}
+                            start={{ x: 0, y: 0 }}
+                            end={{ x: 1, y: 0 }}
+                        >
+                            <Text>
+                                Add Medication
+                                {/* {isSubmitting ? "Adding..." : "Add Medication"} */}
+                            </Text>
+                        </LinearGradient>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text>Cancel</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#f8f9fa", 
+    },
+    headerGradient: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        height: Platform.OS == 'ios' ? 180 : 140,
+    },
+    content: {
+        flex: 1,
+        paddingTop: Platform.OS == 'ios' ? 50 : 30,
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        paddingBottom: 20,
+        zIndex: 1,
+    },
+    backButton: {
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 3,
+    },
+    headerTitle: {
+        fontSize: 28,
+        fontWeight: '700',
+        color: 'white',
+        marginLeft: 15,
+    },
+    formContentContainer: {
+        
+    }
+})
